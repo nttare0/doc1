a5ffb7b684f35886d88f2ce470f106bbf3dda0c3
niyon <niyonexample@gmail.com>
2025-09-03 12:58:09 +0200

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -61,11 +61,15 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
+  const listenOptions: any = {
     port,
     host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  };
+  // SO_REUSEPORT is not supported on Windows; include only on supported platforms
+  if (process.platform !== "win32") {
+    listenOptions.reusePort = true;
+  }
+  server.listen(listenOptions, () => {
     log(`serving on port ${port}`);
   });
 })();
